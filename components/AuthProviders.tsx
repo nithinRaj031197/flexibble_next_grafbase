2b1b5c69bd215e05f9ec2e8d0cdcd11352f9c56f
nithinRaj031197 <nithinraj.official3@gmail.com>
2023-07-12 23:55:43 +0530

    next auth for google added, grafbase config and graphql query and mutation added

--- components/AuthProviders.tsx
+++ components/AuthProviders.tsx
@@ -1,5 +1,43 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { getProviders, signIn } from "next-auth/react";
+
+type Provider = {
+  id: string;
+  name: string;
+  type: string;
+  signinUrl: string;
+  callbackUrl: string;
+  signinUrlParams?: Record<string, string> | null;
+};
+
+type Providers = Record<string, Provider>;
+
 const AuthProviders = () => {
-  return <div>AuthProviders</div>;
+  const [providers, setProviders] = useState<Providers | null>(null);
+
+  useEffect(() => {
+    const fetchProviders = async () => {
+      const res = await getProviders();
+      setProviders(res);
+    };
+
+    fetchProviders();
+  }, []);
+
+  if (providers) {
+    return (
+      <div>
+        {Object.values(providers).map((provider: Provider, i: number) => (
+          <button key={i} onClick={() => signIn(provider?.id)}>
+            {provider?.id}
+          </button>
+        ))}
+      </div>
+    );
+  }
+  return null;
 };
 
 export default AuthProviders;
